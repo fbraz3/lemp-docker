8cd3059c198a8f1028e188ae69705362deb33ca9
Felipe Keller Braz <fbraz3@gmail.com>
2020-03-06 20:31:23 -0300

    added support to choose uid/gid to www-data user/group

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 $(which chmod) 700 /etc/monit/monitrc
 
-#CLEAR TMP FILES
+# CLEAR TMP FILES
 /root/autoclean.sh
 
-#ADD CRON
+# ADD CRON
 CRONFILE="/cronfile.final"
 SYSTEMCRON="/cronfile.system"
 USERCRON="/cronfile"
@@ -19,7 +19,7 @@ if [ -f "$USERCRON" ]; then
 fi
 /usr/bin/crontab $CRONFILE
 
-#DECLARE/SET VARIABLES
+# DECLARE/SET VARIABLES
 PHPVERSION=`cat /PHP_VERSION 2>/dev/null`
 if [ -z "$PHPVERSION" ]; then
     PHPVERSION=`php -v|grep --only-matching --perl-regexp "7\.\\d+" |head -n1`
@@ -29,7 +29,17 @@ if [ -z "$PHPVERSION" ]; then
     PHPVERSION='7.3'
 fi
 
-#SORRY FOR THAT =(
+# SET UID FOR www-data USER
+if  [[ ! -z "$DATA_UID" ]] && [[ $DATA_UID =~ ^[0-9]+$ ]] ; then
+	$(which usermod) -u $DATA_UID www-data;
+fi
+
+# SET GUID FOR www-data USER
+if  [[ ! -z "$DATA_GUID" ]] && [[ $DATA_GUID =~ ^[0-9]+$ ]] ; then
+	$(which groupmod) -g $DATA_GUID www-data;
+fi
+
+# SORRY FOR THAT =(
 if [ -f "/etc/php/fpm/php-fpm.conf" ]; then
     $(which cp) -f /etc/php/fpm/php-fpm.conf /etc/php/$PHPVERSION/fpm/php-fpm.conf
 fi
@@ -42,7 +52,7 @@ if [ -f "/etc/php/fpm/pool.d/www.conf" ]; then
     $(which cp) -f /etc/php/fpm/pool.d/www.conf /etc/php/$PHPVERSION/fpm/pool.d/www.conf
 fi
 
-#POPULATE ENV
+# POPULATE ENV
 echo > /etc/php/$PHPVERSION/fpm/env.conf
 for i in `/usr/bin/env`; do
     PARAM=`echo $i |cut -d"=" -f1`
@@ -50,7 +60,7 @@ for i in `/usr/bin/env`; do
     echo "env[$PARAM]=\"$VAL\"" >> /etc/php/$PHPVERSION/fpm/env.conf
 done
 
-#PUPULATE TEMPLATES
+# POPULATE TEMPLATES
 cp -f /etc/ssmtp/ssmtp.conf.template /etc/ssmtp/ssmtp.conf
 sed -i 's/%MY_HOSTNAME%/'`/bin/hostname`'/g' /etc/ssmtp/ssmtp.conf
 
@@ -60,7 +70,7 @@ $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/php/$PHPVERSION/fpm/php-f
 $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/nginx/sites-available/default
 
 
-#START SERVICES
+# START SERVICES
 /etc/init.d/cron restart
 /etc/init.d/php$PHPVERSION-fpm restart
 /etc/init.d/mysql restart
@@ -68,8 +78,8 @@ $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/nginx/sites-available/def
 sleep 1
 /etc/init.d/monit restart
 
-#Create mariadb user
+# CREATE MARIADB USER
 mysql < /build.sql
 
-#KEEP CONTAINER ALIVE
+# KEEP CONTAINER ALIVE
 /usr/bin/tail -f /var/log/nginx/access.log
