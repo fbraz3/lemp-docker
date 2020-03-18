52dc75521ececa0c434a1ccd4364d8d80f9990a7
Felipe Keller Braz <fbraz3@gmail.com>
2020-03-18 19:41:04 -0300

    support to edit php variables via environment

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -29,12 +29,12 @@ if [ -z "$PHPVERSION" ]; then
     PHPVERSION='7.3'
 fi
 
-# SET UID FOR www-data USER
+# SET CUSTOM ID FOR www-data USER
 if  [[ ! -z "$DATA_UID" ]] && [[ $DATA_UID =~ ^[0-9]+$ ]] ; then
 	$(which usermod) -u $DATA_UID www-data;
 fi
 
-# SET GUID FOR www-data USER
+# SET CUSTOM ID FOR www-data GROUP
 if  [[ ! -z "$DATA_GUID" ]] && [[ $DATA_GUID =~ ^[0-9]+$ ]] ; then
 	$(which groupmod) -g $DATA_GUID www-data;
 fi
@@ -52,14 +52,6 @@ if [ -f "/etc/php/fpm/pool.d/www.conf" ]; then
     $(which cp) -f /etc/php/fpm/pool.d/www.conf /etc/php/$PHPVERSION/fpm/pool.d/www.conf
 fi
 
-# POPULATE ENV
-echo > /etc/php/$PHPVERSION/fpm/env.conf
-for i in `/usr/bin/env`; do
-    PARAM=`echo $i |cut -d"=" -f1`
-    VAL=`echo $i |cut -d"=" -f2`
-    echo "env[$PARAM]=\"$VAL\"" >> /etc/php/$PHPVERSION/fpm/env.conf
-done
-
 # POPULATE TEMPLATES
 cp -f /etc/ssmtp/ssmtp.conf.template /etc/ssmtp/ssmtp.conf
 sed -i 's/%MY_HOSTNAME%/'`/bin/hostname`'/g' /etc/ssmtp/ssmtp.conf
@@ -69,6 +61,44 @@ $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/php/$PHPVERSION/fpm/pool.
 $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/php/$PHPVERSION/fpm/php-fpm.conf
 $(which sed) -i 's/%PHP_VERSION%/'$PHPVERSION'/g' /etc/nginx/sites-available/default
 
+# POPULATE VARIABLES
+echo > /etc/php/$PHPVERSION/fpm/env.conf
+echo > /etc/php/$PHPVERSION/fpm/overrides.conf
+echo > /etc/php/$PHPVERSION/fpm/pool-overrides.conf
+echo > /etc/php/$PHPVERSION/fpm/phpconf.conf
+for i in `/usr/bin/env`; do
+    PARAM=`echo $i |cut -d"=" -f1`
+    VAL=`echo $i |cut -d"=" -f2`
+
+    if [[ "$PARAM" == "_" ]]; then
+        continue
+    fi
+
+    if [[ $PARAM =~ ^PHPADMIN_.+ ]]; then
+        PHPPARAM=`echo $PARAM |sed 's/PHPADMIN_//g' | awk '{print tolower($0)}'`
+        echo "PHPADMIN   :: $PHPPARAM => $VAL"
+        echo "php_admin_value[$PHPPARAM] =\"$VAL\"" >> /etc/php/$PHPVERSION/fpm/phpconf.conf
+
+    elif [[ $PARAM =~ ^PHPFLAG_.+ ]]; then
+        PHPPARAM=`echo $PARAM |sed 's/PHPFLAG_//g' | awk '{print tolower($0)}'`
+        echo "PHPFLAG    :: $PHPPARAM => $VAL"
+        echo "php_flag[$PHPPARAM]=\"$VAL\"" >> /etc/php/$PHPVERSION/fpm/phpconf.conf
+
+    elif [[ $PARAM =~ ^FPMCONFIG_.+ ]]; then
+        FPMPARAM=`echo $PARAM |sed 's/FPMCONFIG_//g' | sed 's/__/./g' | awk '{print tolower($0)}'`
+        echo "FPMCONFIG  :: $FPMPARAM => $VAL"
+        echo "$FPMPARAM = $VAL" >> /etc/php/$PHPVERSION/fpm/overrides.conf
+
+    elif [[ $PARAM =~ ^POOLCONFIG_.+ ]]; then
+        FPMPARAM=`echo $PARAM |sed 's/POOLCONFIG_//g' | sed 's/__/./g' | awk '{print tolower($0)}'`
+        echo "POOLCONFIG :: $FPMPARAM => $VAL"
+        echo "$FPMPARAM = $VAL" >> /etc/php/$PHPVERSION/fpm/pool-overrides.conf
+
+    elif [[ $PARAM =~ ^[a-zA-Z0-9_]+$ ]]; then
+        echo "ENV :: $PARAM => $VAL"
+        echo "env[$PARAM]=\"$VAL\"" >> /etc/php/$PHPVERSION/fpm/env.conf
+    fi
+done
 
 # START SERVICES
 /etc/init.d/cron restart
