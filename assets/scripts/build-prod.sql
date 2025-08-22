9a1615ff03ce7ae902bc421da21d8e910a7f1039
Felipe Keller Braz <fbraz3@gmail.com>
2025-08-22 16:18:43 -0300

    fix: correct SQL script for user and database setup in production

--- assets/scripts/build-prod.sql
+++ assets/scripts/build-prod.sql
@@ -2,4 +2,3 @@ CREATE USER 'root'@'%';
 GRANT ALL PRIVILEGES ON *.* to 'root'@'%' WITH GRANT OPTION;
 DELETE FROM mysql.user WHERE user='root' AND host='localhost';
 FLUSH PRIVILEGES;
-CREATE DATABASE app;
